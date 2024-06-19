0832abd09ed5c075b5c30618723382ae33c2cf44
kinar <kinar8606@gmail.com>
2024-06-19 19:43:56 +0300

    few changes

--- test.py
+++ test.py
@@ -65,54 +65,54 @@
 
 
 
-        elif self.path == '/additem':
-            current_user_id = get_user_id_from_cookies(self.headers)
+        # elif self.path == '/additem':
+        #     current_user_id = get_user_id_from_cookies(self.headers)
 
-            if not current_user_id:
-                self._send_response("Unauthorized - User not logged in")
-                return
+        #     if not current_user_id:
+        #         self._send_response("Unauthorized - User not logged in")
+        #         return
 
-            content_type, pdict = parse_options_header(self.headers['Content-Type'])
-            if content_type == 'multipart/form-data':
-                boundary = pdict['boundary'].encode('utf-8')
-                content_length = int(self.headers['Content-Length'])
-                parser = MultipartParser(self.rfile, boundary, content_length)
+        #     content_type, pdict = parse_options_header(self.headers['Content-Type'])
+        #     if content_type == 'multipart/form-data':
+        #         boundary = pdict['boundary'].encode('utf-8')
+        #         content_length = int(self.headers['Content-Length'])
+        #         parser = MultipartParser(self.rfile, boundary, content_length)
                 
-                form_data = {}
-                file_data = None
+        #         form_data = {}
+        #         file_data = None
                 
-                for part in parser:
-                    if part.name == 'item_name':
-                        form_data['item_name'] = part.value.decode('utf-8')
-                    elif part.name == 'description':
-                        form_data['description'] = part.value.decode('utf-8')
-                    elif part.name == 'photo' and part.filename:
-                        file_data = part
+        #         for part in parser:
+        #             if part.name == 'item_name':
+        #                 form_data['item_name'] = part.value.decode('utf-8')
+        #             elif part.name == 'description':
+        #                 form_data['description'] = part.value.decode('utf-8')
+        #             elif part.name == 'photo' and part.filename:
+        #                 file_data = part
 
-                item_name = form_data.get('item_name')
-                description = form_data.get('description')
+        #         item_name = form_data.get('item_name')
+        #         description = form_data.get('description')
 
-                if file_data:
-                    photo_name = os.path.basename(file_data.filename)
-                    photo_path = os.path.join('uploads', photo_name)
-                    with open(photo_path, 'wb') as f:
-                        f.write(file_data.file.read())
-                else:
-                    photo_path = None
+        #         if file_data:
+        #             photo_name = os.path.basename(file_data.filename)
+        #             photo_path = os.path.join('uploads', photo_name)
+        #             with open(photo_path, 'wb') as f:
+        #                 f.write(file_data.file.read())
+        #         else:
+        #             photo_path = None
 
-                list_id = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('list_id', [''])[0]
+        #         list_id = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('list_id', [''])[0]
 
-                try:
-                    conn = mysql.connector.connect(**mysql_config)
-                    cursor = conn.cursor()
-                    query = "INSERT INTO items (item_name, description, photo_path, list_id) VALUES (%s, %s, %s, %s)"
-                    cursor.execute(query, (item_name, description, photo_path, list_id))
-                    conn.commit()
-                    self._redirect(f'/list.html?list_id={list_id}')
-                except mysql.connector.Error as err:
-                    self._send_response(f"Error: {err}")
-                finally:
-                    cursor.close()
-                    conn.close()
-                else:
-                         self.send_error(404, 'File Not Found')
+        #         try:
+        #             conn = mysql.connector.connect(**mysql_config)
+        #             cursor = conn.cursor()
+        #             query = "INSERT INTO items (item_name, description, photo_path, list_id) VALUES (%s, %s, %s, %s)"
+        #             cursor.execute(query, (item_name, description, photo_path, list_id))
+        #             conn.commit()
+        #             self._redirect(f'/list.html?list_id={list_id}')
+        #         except mysql.connector.Error as err:
+        #             self._send_response(f"Error: {err}")
+        #         finally:
+        #             cursor.close()
+        #             conn.close()
+        #         else:
+        #                  self.send_error(404, 'File Not Found')
